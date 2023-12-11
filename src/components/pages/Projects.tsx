d9ce734276f19e397fe81bb417aec49ec69a0f30
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2023-12-11 14:04:33 +0600

    update data property type of project page

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -3,44 +3,47 @@ import HandBurger from '../temp/HandBurger';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
+// Define the type/interface for your project data
+interface Project {
+  id: number;
+  name: string;
+  title: string;
+  image: string;
+  // Add any other properties you expect here
+}
 
 const Projects = () => {
+  const [datas, setDatas] = useState<Project[]>([]); // Use the defined type/interface
 
-  const [datas,setDatas]=useState([])
-
-  useEffect(()=>{
-    axios.get('https://portfolio-backend-orpin-eight.vercel.app/projects')
-    .then(res=>{
-      console.log(res.data);
-      setDatas(res.data);
-    })
-  },[])
-
-
-
-
+  useEffect(() => {
+    axios.get<Project[]>('https://portfolio-backend-orpin-eight.vercel.app/projects')
+      .then(res => {
+        console.log(res.data);
+        setDatas(res.data);
+      })
+  }, []);
 
   return (
     <>
-    <HandBurger/>
-    <div className='projectsWrapper'>
-      {datas && datas.map((one)=>{
-        console.log(one)
-        return(
-          <div className='temporay' key={one.id}>
-            <Link style={{textDecoration:"none"}} state={one} to={one.name}>
-              <div className='project'>
-                <div className="bgImage" style={{backgroundImage:`url(${one.image})`}}></div>
-                <h2>{one.name}</h2>
-                <h3>{one.title}</h3> 
-              </div>
-            </Link>
-          </div>
-        )
-      })}
-    </div>
+      <HandBurger />
+      <div className='projectsWrapper'>
+        {datas && datas.map((one) => {
+          console.log(one)
+          return (
+            <div className='temporay' key={one.id}>
+              <Link style={{ textDecoration: "none" }} state={one} to={one.name}>
+                <div className='project'>
+                  <div className="bgImage" style={{ backgroundImage: `url(${one.image})` }}></div>
+                  <h2>{one.name}</h2>
+                  <h3>{one.title}</h3>
+                </div>
+              </Link>
+            </div>
+          )
+        })}
+      </div>
     </>
   )
 }
 
-export default Projects
+export default Projects;
